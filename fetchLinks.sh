04235def8b4f658140abc023e3f32d63ae6f9b5f
Arana_Jayavihan <it21038150@my.sliit.lk>
2023-03-09 03:15:50 -0500

    fetch script final

--- fetchLinks.sh
+++ fetchLinks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 {
-	xh -d https://data.phishtank.com/data/online-valid.csv
-	
+	date=$(date)
+	xh -d https://data.phishtank.com/data/online-valid.csv >> phishlinks.csv
+	sleep 3
+	git add phishlinks.csv
+	git commit -m "Update $date"
+	git push
 }
